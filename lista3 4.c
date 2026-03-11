2b2c022d5cc86d4023dfbae02004a25f490a4970
leodm5674 <98219987+leodm5674@users.noreply.github.com>
2026-03-10 22:13:15 -0300

    Add files via upload

--- lista3 4.c
+++ lista3 4.c
@@ -4,12 +4,15 @@
 #include <stdio.h>
 #include <math.h>
 int main(int argc, char **argv)
-{ double valor1, valor2,  valor3, media;
+
+{ double  media;
+	int valor1, valor2,  valor3;
 	printf("Digite 3 numeros \n");
-	scanf("%lf %lf %lf", &valor1, &valor2, &valor3);
+	scanf("%d %d %d", &valor1, &valor2, &valor3);
 	
-	
-	
+	media = (double) (valor1 + valor2 + valor3) / 3;
+	 
+	 printf("O resultado será de: %lf \n", media);
 	
 	
 	return 0;
